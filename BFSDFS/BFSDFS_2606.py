c0989adff69fa69b9954bdc114128364bdcbf08a
RyuYS-17 <60517477+RyuYS-17@users.noreply.github.com>
2021-04-16 17:20:44 +0900

    다시 풀음

--- BFSDFS/BFSDFS_2606.py
+++ BFSDFS/BFSDFS_2606.py
@@ -1,29 +1,29 @@
-def solution(N, M):
-    graph = []
-    edges = []
+def fill_networks(networks, M):
     for _ in range(M):
-        edges.append(list(map(int, input().split())))
-    for i in range(N):
-        temp = []
-        for edge in edges:
-            if (i+1)==edge[0]:
-                temp.append(edge[1])
-            if (i+1)==edge[1]:
-                temp.append(edge[0])
-        graph.append(temp)
-    
-    virus = []
-    stack = []
-    stack.append(1)
-    while stack:
-        node = stack.pop(0)
-        if node not in virus:
-            virus.append(node)
-            for link in graph[(node-1)]:
-                stack.append(link)
-    return len(virus)-1
+        A, B = map(int, input().split())
+        networks[(A-1)].add(B)
+        networks[(B-1)].add(A)
 
-if __name__ == "__main__":
+def virus():
+    from collections import deque
     N = int(input())
     M = int(input())
-    print(solution(N, M))
+
+    networks = [set() for _ in range(N)]
+    fill_networks(networks, M)
+    
+    node_start = 1
+    queue = deque()
+    infested = list()
+    queue.append(node_start)
+    
+    while(queue):
+        computer = queue.popleft()
+        if computer not in infested:
+            infested.append(computer)
+            for link in networks[(computer-1)]:
+                queue.append(link)
+    return len(infested)-1
+
+if __name__ == "__main__":
+    print(virus())
