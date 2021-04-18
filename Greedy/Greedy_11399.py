c4de3920062aab68d40465511fda27853daee79c
RyuYS-17 <60517477+RyuYS-17@users.noreply.github.com>
2021-04-18 23:12:41 +0900

    그리디 2/50

--- Greedy/Greedy_11399.py
+++ Greedy/Greedy_11399.py
@@ -4,9 +4,9 @@ def solution():
     waitingTime = sorted(list(map(int,input().split())))
     totalTime = 0; i = 0
     for time in waitingTime:
-        totalTime += (N-i)*time
+        total += (N-i)*time
         i += 1
-    return totalTime
+    return total
 
 if __name__ == "__main__":
     print(solution())
