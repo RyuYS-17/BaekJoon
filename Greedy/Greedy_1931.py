a7b479864e6465a6e93043d9f0faaedc1a6ca48b
RyuYS-17 <60517477+RyuYS-17@users.noreply.github.com>
2021-05-22 15:53:35 +0900

    그리디 5/50

---
+++ Greedy/Greedy_1931.py
@@ -0,0 +1,19 @@
+def solution():
+    from sys import stdin
+    
+    N = int(stdin.readline())
+    timetable = [tuple(map(int, stdin.readline().split())) for _ in range(N)]
+    timetable.sort(key= lambda x: (x[1], x[0]))
+
+    count = 1
+    pivot = timetable[0][1]
+    
+    for x in timetable[1:]:
+        if x[0] >= pivot:
+            pivot = x[1]
+            count += 1
+            
+    return count
+
+if __name__ == "__main__":
+    print(solution())
